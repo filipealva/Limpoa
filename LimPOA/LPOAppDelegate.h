93137180fca77a2aadde369ebe240f350c8b6290
Filipe Alvarenga <ofilipealvarenga@gmail.com>
2014-05-13 21:01:59 -0300

    Initial Commit

---
+++ LimPOA/LPOAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  LPOAppDelegate.h
+//  LimPOA
+//
+//  Created by Filipe Alvarenga on 5/13/14.
+//  Copyright (c) 2014 Filipe Alvarenga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ LimPOA/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LimPOA
+//
+//  Created by Filipe Alvarenga on 5/13/14.
+//  Copyright (c) 2014 Filipe Alvarenga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LPOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LPOAppDelegate class]));
+    }
+}
