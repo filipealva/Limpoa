9f3b1fa1a2824feb2429adeac0501360f7a8f990
Filipe Alvarenga <ofilipealvarenga@gmail.com>
2014-07-03 08:59:47 -0300

    Some strings adjustments

--- LimPOA/Controller/LPOCustomTabViewController.m
+++ LimPOA/Controller/LPOCustomTabViewController.m
@@ -22,11 +22,6 @@
     [[self.tabBar.items objectAtIndex:1] setTitle:NSLocalizedString(@"ecopoint_list_title", nil)];
     [[self.tabBar.items objectAtIndex:2] setTitle:NSLocalizedString(@"dump_list_title", nil)];
     [[self.tabBar.items objectAtIndex:3] setTitle:NSLocalizedString(@"container_list_title", nil)];
-    
-//    //Adjust image position to no-title item position.
-//    for (UITabBarItem *item in self.tabBar.items){
-//        item.imageInsets = UIEdgeInsetsMake(5, 0, -5, 0);
-//    }
 }
 
 @end
